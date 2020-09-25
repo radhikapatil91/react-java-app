28d6640af5b073afbe28b20ba9321b5340845322
radhika.patil <radhika.til@gmail.com>
2020-09-25 15:25:21 +0530

    Updates

---
+++ precache-manifest.e24aed5283e37bda780f7677747612ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53747ea99b2bcea3300c",
+    "url": "/my-app/static/css/main.d3988b01.chunk.css"
+  },
+  {
+    "revision": "53747ea99b2bcea3300c",
+    "url": "/my-app/static/js/main.cc73c3a8.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "558871eb6eef68ea8c61",
+    "url": "/my-app/static/css/2.11e459ba.chunk.css"
+  },
+  {
+    "revision": "558871eb6eef68ea8c61",
+    "url": "/my-app/static/js/2.ae8c7071.chunk.js"
+  },
+  {
+    "revision": "8785eb8feeb2099388dfa3c0356fd6cb",
+    "url": "/my-app/index.html"
+  }
+];
